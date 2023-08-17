3029133abda6a8d63b83a91d2a663bd3d85a6817
Jason Underhill <jason@junderhill.com>
2023-08-17 14:28:56 +0100

    Tweaks for publishing to PyPi

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="py-find-aws-region",
+    name="find_aws_regions",
     version="0.0.1",
     author="Jason Underhill",
     author_email="jason@junderhill.com",
     description="Find the closest/best AWS region from your code bassed on latency",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="github.com/junderhill/py-find-aws-region",
+    url="https://github.com/junderhill/py-find-aws-region",
     package_dir = {'': 'src'},
     packages=setuptools.find_packages(where='src'),
     classifiers=[
@@ -19,10 +19,9 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         ],
-    package_dir = {'': 'src'},
-    packages=setuptools.find_packages(where='src'),
     python_requires='>=3.6',
     install_requires=[
         'boto3',
+        'requests',
     ],
 )
